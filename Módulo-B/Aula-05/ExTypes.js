314e8edf871e225ec358cd77b6f14f7bd093bd56
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-12-29 09:16:27 -0300

    Create ExTypes.js

---
+++ Módulo-B/Aula-05/ExTypes.js
@@ -0,0 +1,17 @@
+function ex001() {
+    var answer = 5
+    type = (typeof answer)
+    alert(type + ': ' + answer)
+
+    var answer = 5.8
+    type = (typeof answer)
+    alert(type + ': ' + answer)
+
+    var answer = 'Letras'
+    type = (typeof answer)
+    alert(type + ': ' + answer)
+
+    var answer = true
+    type = (typeof answer)
+    alert(type + ': ' + answer)
+}
