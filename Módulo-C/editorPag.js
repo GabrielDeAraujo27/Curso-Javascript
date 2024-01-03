01002415bf890c7d822530082a7555bbb507d57a
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2024-01-03 20:53:46 -0300

    Update editorPag.js

--- Módulo-C/editorPag.js
+++ Módulo-C/editorPag.js
@@ -1,6 +1,15 @@
-function ex001() {
+async function ex001() {
     edit = prompt("O que você quer adicionar na página?")
-    edit = "<br>" + "<p>" + edit + "</p>"
-    div = document.getElementById("ex01")
-    div.innerHTML = edit
+    edit = "<p>" + edit + "</p>"
+    ex = document.getElementById("ex01")
+    ex.style.background = "green"
+    ex.innerHTML = edit
+}
+
+async function ex002() {
+    edi = prompt("O que você quer adicionar na página?")
+    edi = "<p>" + edi + "</p>"
+    exe = document.querySelector('div#ex02')
+    exe.style.color = "blue"
+    exe.innerHTML = edi
 }
