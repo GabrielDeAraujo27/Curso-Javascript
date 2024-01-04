eb7c343318dc38ce0ec0ebdf0e96fb395dfef2b1
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2024-01-04 10:36:26 -0300

    Update testClick.js

--- Módulo-C/Aula-10/testClick.js
+++ Módulo-C/Aula-10/testClick.js
@@ -1,15 +1,12 @@
-function ex003() {
-    var botto = window.document.getElementById('ex03')
-    botto.style.backgroundColor = "blue"
-    botto.innerHTML = "<h2>Entrou!</h2>"
+function entrar() {
+    bott.style.backgroundColor = "blue"
+    bott.innerHTML = "<h2>Entrou!</h2>"
 }
-function ex004() {
-    var botto = window.document.getElementById('ex03')
-    botto.style.backgroundColor = "green"
-    botto.innerHTML = "<h2>Clicou!</h2>"
+function clicar() {
+    bott.style.backgroundColor = "green"
+    bott.innerHTML = "<h2>Clicou!</h2>"
 }
-function ex005() {
-    var botto = window.document.getElementById('ex03')
-    botto.style.backgroundColor = "yellow"
-    botto.innerHTML = "<h2>Saiu!</h2>"
+function sair() {
+    bott.style.backgroundColor = "yellow"
+    bott.innerHTML = "<h2>Saiu!</h2>"
 }
