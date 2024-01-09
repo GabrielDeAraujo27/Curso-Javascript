cde545a453fa7203ab04f2626c5e3a7e08d477d3
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2024-01-09 12:36:36 -0300

    Create brasil.js

---
+++ Módulo-D/ex001/brasil.js
@@ -0,0 +1,9 @@
+function bras() {
+    nac = prompt('Em que país você nasceu?')
+    nac.toLowerCase
+    if (nac == 'brasil'){
+        alert('Você é brasileiro.')
+    } else {
+        alert('Você é estrangeiro.')
+    }
+}
